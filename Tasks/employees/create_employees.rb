2cc4b070beb44817d15b2166fd9ed0096799cd65
vkozin <vladiksystem@gmail.com>
2018-05-15 13:14:26 +0300

    ref

--- Tasks/employees/create_employees.rb
+++ Tasks/employees/create_employees.rb
@@ -3,7 +3,7 @@ require_relative 'salaried_employee'
 require_relative 'hourly_employee'
 
 
-jane = SalariedEmployee.new("Jane Doe", 50000)
+jane = SalariedEmployee.new('Jane Doe', 50000)
 jane.print_pay_stub
 
 angela = HourlyEmployee.security_guard("Angela Matthews")
