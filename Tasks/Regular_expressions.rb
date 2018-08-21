115eff2d3463743292a11c0e500e33e45f8aa804
vkozin <vladiksystem@gmail.com>
2018-08-21 11:23:22 +0300

    regex

---
+++ Tasks/Regular_expressions.rb
@@ -0,0 +1,32 @@
+# + one or more and d - digit
+puts /\d+/ === "123" #true
+
+# 172.16.0.0 to 172.31.255.255
+IP_ADDRESS_REGEX = /^((?:(?:^|\.)(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])){4})$/
+
+def is_valid_ip_address? ip
+  ip =~ IP_ADDRESS_REGEX
+end
+
+p is_valid_ip_address?("999.16.0.0") ? "valid" : "invalid"
+p is_valid_ip_address?("172.16.0.0") ? "valid" : "invalid"
+p is_valid_ip_address?("172.131.255.255") ? "valid" : "invalid"
+p is_valid_ip_address?("172.31.255.256") ? "valid" : "invalid"
+p '~' * 120
+
+# /i modifier (ignores case of [a-zA-Z])
+regex = /^[a-z0-9]+@[a-z0-9]+\.[a-z]+$/i
+string = 'Install@gmail.com'
+p string =~ regex ? "Valid : #{string}" : "Invalid Mail"
+
+
+r = /^[a-z\d\.]+@[a-z\d\.]+\.[a-z]+$/
+s = 'install2012.vv@smtp.gmail.com'
+p s =~ r ? "Valid : #{s}" : "Invalid Mail"
+
+def split_into_words(str)
+  str.split(/(?=[A-Z])/)
+end
+puts split_into_words("camelCasingTestFooBar")
+      .map {|x| x.downcase}
+      .join('*')
